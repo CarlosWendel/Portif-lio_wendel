3c3fadfaf8d01a65a0d3519fb12816483d7e0062
CarlosWendel <wendeluzumak824@gmail.com>
2026-06-10 21:03:02 -0300

    fix: corrige download do curriculo

--- src/components/header/DawnloadCV.tsx
+++ src/components/header/DawnloadCV.tsx
@@ -3,14 +3,14 @@ import { ArrowLineDownIcon   } from "@phosphor-icons/react";
 export default function DownloadCV() {
   return (
     <a
-      href="../../../public/Curriculo_Desenvolvedor_FrontEnd_CarlsoWendel.pdf"
+      href="/Curriculo-Wendel.pdf"
       download="Curriculo_Desenvolvedor_FrontEnd_CarlsoWendel.pdf"
       target="_blank"
       rel="noopener noreferrer"
       className="px-4 py-2  text-white rounded  transition-colors duration-300 flex items-center gap-2 border border-violet-400 hover:bg-violet-400 hover:text-white"
     >
       <ArrowLineDownIcon   size={20} />
-      <button> Currículo</button>
+        <span>Currículo</span>
     </a>
   );
 }
